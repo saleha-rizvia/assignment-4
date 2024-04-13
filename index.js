985b8d93e4ff18536a08eb23c6f85a491e0a7f0f
saleharizvia <shahidhameed17366@gmail.com>
2024-04-13 15:08:18 +0500

    Assignment 4

---
+++ index.js
@@ -0,0 +1,118 @@
+"use strict";
+// Question 1
+let greeting = ("hello world");
+console.log("greeting");
+// Question 2
+let num1 = 5;
+let num2 = 2;
+console.log("Addition", num1 + num2);
+console.log("Subtraction", num1 - num2);
+console.log("Multipy", num1 * num2);
+console.log("Division", num1 / num2);
+// Question 3
+let a = 1;
+let b = 2;
+[a, b] = [b, a];
+console.log("A", a);
+console.log("B", b);
+//Question 4
+let message = "hello";
+/* message = 10;
+//Type 'number' is not assignable to type 'string'.ts(2322)
+let message: string*/
+console.log(message);
+// Question 5
+let num3 = 6;
+let num4 = 3;
+console.log("num%num4");
+// Question 6
+let counter = 5;
+counter++;
+console.log(counter);
+counter += 10;
+console.log(counter);
+// Question 7
+let aa = true;
+let bb = false;
+let cc = true;
+// AND
+console.log("&a");
+console.log("&B");
+console.log("&C");
+// OR
+console.log("||a");
+console.log("||b");
+console.log("||c");
+// NOT
+console.log("!a");
+console.log("!b");
+console.log("!c");
+// Question 8
+let D = 10;
+console.log(D);
+D += 2;
+console.log("+=", D);
+D -= 6;
+console.log("-=", D);
+D *= 2;
+console.log("*=", D);
+D /= 2;
+console.log("/*=", D);
+// Question 9
+let number = 1;
+if (number % 2 == 0) {
+    console.log("Number is even");
+}
+else {
+    console.log("Number is ODD");
+}
+;
+// Question 10
+let age = 43;
+if (age >= 18) {
+    console.log("Eligible for voting");
+}
+else {
+    console.log("Not Eligible for votting");
+}
+// Question 12
+let a1 = 6;
+let b2 = 8;
+let c2 = 10;
+let maximum = Math.max(a1, b2, c2);
+console.log("Maximum Digit is", maximum);
+// Question 13
+let leap_year = 2001;
+if (leap_year % 4 == 0) {
+    console.log("It's Leap Year", leap_year);
+}
+else {
+    console.log("non-leap year", leap_year);
+}
+// Question 14
+let Fahrenheit = 120;
+let celsius = (Fahrenheit - 32) * 5 / 9;
+console.log("Temperature in Celsius", celsius);
+// Question 15
+let Number1 = 0;
+if (Number1 >= 0) {
+    console.log("Number is positive", Number1);
+}
+else if (Number1 < 0) {
+    console.log("Number is Negitive", Number1);
+}
+else {
+    console.log("Number is zero", Number1);
+}
+// Question 16
+let table_10 = 10;
+console.log("10*1=", table_10 * 1);
+console.log("10*2=", table_10 * 2);
+console.log("10*3=", table_10 * 3);
+console.log("10*4=", table_10 * 4);
+console.log("10*5=", table_10 * 5);
+console.log("10*6=", table_10 * 6);
+console.log("10*7=", table_10 * 7);
+console.log("10*8=", table_10 * 8);
+console.log("10*9=", table_10 * 9);
+console.log("10*10=", table_10 * 10);
